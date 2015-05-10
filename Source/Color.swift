f9041a36a05397f162072f71e993522a13914d30
Bartek Chlebek <bartek.public@gmail.com>
2015-05-11 01:06:49 +0200

    Fix for a crash on ARM device

--- Source/Color.swift
+++ Source/Color.swift
@@ -4,6 +4,7 @@ public struct Color {
   var red: UInt8
   var green: UInt8
   var blue: UInt8
+  private let z: UInt8 = 0 // fix for issue described here http://stackoverflow.com/questions/30157538/exc-arm-da-align-on-device
   
   public init(red: UInt8, green: UInt8, blue: UInt8) {
     self.red = red
